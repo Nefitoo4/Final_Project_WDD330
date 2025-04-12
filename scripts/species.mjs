aee61484caeee275684e2720f70d7c1d4198ff7d
Nefitoo4 <nefizaldana@gmail.com>
2025-04-12 10:52:08 -0600

    cards

--- scripts/species.mjs
+++ scripts/species.mjs
@@ -1,8 +1,7 @@
-// Select the container where the species will be displayed
-const speciesContainer = document.querySelector("#species-container");
-
 // Function to load species data from the JSON file
 export async function loadSpecies() {
+  // Select the container where the species will be displayed
+  const speciesContainer = document.querySelector("#species-container");
   try {
     // Load the JSON file
     const response = await fetch("./data/species.json");
@@ -11,10 +10,20 @@ export async function loadSpecies() {
     // Generate HTML for each species and append to the container
     species.forEach((animal) => {
       const speciesCard = document.createElement("div");
-      speciesCard.classList.add("species-card", "text-center", "mb-4");
+      speciesCard.classList.add(
+        "card",
+        "mb-4",
+        "mx-auto",
+        "border-primary",
+        "text-primary",
+        "bg-transparent"
+      );
+      speciesCard.style.width = "18rem";
+      speciesCard.style.padding = "16px";
+
       speciesCard.innerHTML = `
-            <img src="${animal.image}" alt="${animal.name}" class="img-fluid mb-2"/>
-            <h5>${animal.name}</h5>`;
+            <img src="${animal.image}" alt="${animal.alt}" class="card-img-top rounded mb-3 img-fluid" style="height: 200px; object-fit: cover;"/>
+            <h5 class="card-title text-center">${animal.name}</h5>`;
 
       // Add the card to the container
       speciesContainer.appendChild(speciesCard);
