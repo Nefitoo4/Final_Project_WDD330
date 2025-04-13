a4f7906877063bc1da5f49964ebef1a90c836022
Nefitoo4 <nefizaldana@gmail.com>
2025-04-12 19:01:18 -0600

    API_1

---
+++ scripts/apiAnimals.mjs
@@ -0,0 +1,51 @@
+const url = "https://animals7.p.rapidapi.com/api/animals?animal=Tiger";
+const options = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "38b719fdd9msh131ad99377d551ep1a7e6cjsnca4b3164b4ae",
+    "x-rapidapi-host": "animals7.p.rapidapi.com",
+  },
+};
+
+export async function fetchAnimalData() {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+    displayAnimals(data);
+  } catch (error) {
+    console.error("Error fetching animal data:", error);
+  }
+}
+
+function displayAnimals(data) {
+  const container = document.querySelector("#species-container");
+  container.innerHTML = ""; // Clear previous content
+
+  data.forEach((animal) => {
+    const card = document.createElement("div");
+    card.classList.add(
+      "card",
+      "mb-4",
+      "mx-auto",
+      "border-primary",
+      "text-primary",
+      "bg-transparent"
+    );
+    card.style.width = "18rem";
+
+    card.innerHTML = `
+            <img src="${animal.image}" alt="${animal.alt}" class="card-img-top rounded mb-3 img-fluid" style="height: 200px;"/>
+            <div class="card-body">
+                <h5 class="card-title text-center">${animal.name}</h5>
+                <p class="text-center"><strong>Scientific Name:</strong>${animal.scientific_name}</p>
+                <p class="text-center"><strong>Habitat:</strong>${animal.habitat}</p>
+                <p class="text-center"><strong>Description:</strong>${animal.description}</p>
+            </div>`;
+
+    container.appendChild(card);
+  });
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,9 @@
-//Animal API Fetch
-// const url = "https://animals7.p.rapidapi.com/api/animals?animal=Tiger";
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "x-rapidapi-key": "38b719fdd9msh131ad99377d551ep1a7e6cjsnca4b3164b4ae",
-//     "x-rapidapi-host": "animals7.p.rapidapi.com",
-//   },
-// };
-
-// try {
-//   const response = await fetch(url, options);
-//   const result = await response.text();
-//   console.log(result);
-// } catch (error) {
-//   console.error(error);
-// }
+import { fetchAnimalData } from "./apiAnimals.mjs";
+//Call the function to fetch animal data
+fetchAnimalData();
 
 //Import the loadSpecies function form species.mjs
-import { loadSpecies } from "./species.mjs";
+// import { loadSpecies } from "./species.mjs";
 
-//Call the function to load species data
-loadSpecies();
+// //Call the function to load species data
+// loadSpecies();
